af6a7ed397eb485f10001c4148d81e1f44af37a1
deniii1753 <92589022+deniii1753@users.noreply.github.com>
2022-02-27 01:30:50 +0200

    Add button click animation

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ generateButtonElement.addEventListener('click', generateButtonHandler);
 function generateButtonHandler() {
     const asciiCodes = getCodes();
 
-    if(!asciiCodes.length){
+    if (!asciiCodes.length) {
+        resultElement.classList.add('invalid-password');
         resultElement.value = 'Please select at least one option.';
         return;
     }
@@ -21,25 +22,29 @@ function generateButtonHandler() {
     const inputPasswordLengthValue = Number(passwordLengthElement.value);
     const asciiCodesLength = asciiCodes.length;
 
-    for(let i = 0; i < inputPasswordLengthValue; i++){
+    for (let i = 0; i < inputPasswordLengthValue; i++) {
         const generatedIndex = generateIndex(asciiCodesLength);
 
         generatedPassword.push(String.fromCharCode(asciiCodes[generatedIndex]));
     }
 
+    if(resultElement.classList.contains('invalid-password')){
+        resultElement.classList.remove('invalid-password');
+    }
+
+    resultElement.classList.add('valid-password');
     resultElement.value = generatedPassword.join('');
 
-    if(selectPasswordInputElement.checked){
-        resultElement.focus();
+    if (selectPasswordInputElement.checked) {
         resultElement.select();
     }
 }
 
-function generateIndex(indexLimit){    
-    let index = Math.floor(Math.random()*100);
+function generateIndex(indexLimit) {
+    let index = Math.floor(Math.random() * 100);
 
-    while(index >= indexLimit){
-        index = Math.floor(Math.random()*100);
+    while (index >= indexLimit) {
+        index = Math.floor(Math.random() * 100);
     }
 
     return index;
@@ -47,30 +52,30 @@ function generateIndex(indexLimit){
 function getCodes() {
     const asciiSymbols = [];
 
-    if(incLowerCaseInputElement.checked){
-        for(let i = 97; i <= 122; i++){
+    if (incLowerCaseInputElement.checked) {
+        for (let i = 97; i <= 122; i++) {
             asciiSymbols.push(i);
         }
     }
 
-    if(incUpperCaseInputElement.checked){
-        for(let i = 65; i <= 90; i++){
+    if (incUpperCaseInputElement.checked) {
+        for (let i = 65; i <= 90; i++) {
             asciiSymbols.push(i);
         }
     }
 
-    if(includeNumbersInputElement.checked){
-        for(let i = 48; i <= 57; i++){
+    if (includeNumbersInputElement.checked) {
+        for (let i = 48; i <= 57; i++) {
             asciiSymbols.push(i);
         }
     }
 
-    if(includeSymbolsInputElement.checked){
-        for(let i = 33; i <= 47; i++){
+    if (includeSymbolsInputElement.checked) {
+        for (let i = 33; i <= 47; i++) {
             asciiSymbols.push(i);
         }
 
-        for(let i = 58; i <= 64; i++){
+        for (let i = 58; i <= 64; i++) {
             asciiSymbols.push(i);
         }
     }
